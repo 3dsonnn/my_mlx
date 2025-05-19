4b4197b645cb410136104f879a81ab078360c5b4
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-05-19 18:40:41 +0100

    my_mlx_new_img.c function to create a new image and pass the size of it to the size t_point variable inside the struct t_img instance

---
+++ my_mlx_new_img.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_new_img.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 11:12:48 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/16 11:16:01 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "my_mlx.h"
+
+void	my_mlx_new_img(void *mlx, t_img *img, t_point size)
+{
+	if (!mlx || !img || size.x <= 0 || size.y <= 0)
+		return ;
+    my_mlx_init_img(img);
+    if (size.x <= 0 || size.y <= 0)
+		return ;
+	img->img = mlx_new_image(mlx, size.x, size.y);
+	if (!img->img)
+		return ;
+	img->size = size;
+}
