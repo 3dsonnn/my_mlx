9eb66b01fd845665e2a9f1067df13206af8f8a87
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-05-19 18:22:57 +0100

    my_mlx_color_img.c to fulfill an image with a specific color passed as argument

---
+++ my_mlx_color_img.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_color_img.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 17:42:36 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/19 17:43:40 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "my_mlx.h"
+
+void	my_mlx_color_img(t_img *img, int color)
+{
+	t_point	iter;
+
+	if (!img || !img->addr || img->size.y <= 0 || img->size.x <= 0)
+		return ;
+	iter = (t_point){-1, -1};
+	while (++iter.y < img->size.y)
+	{
+		iter.x = -1;
+		while (++iter.x < img->size.x)
+			my_mlx_pixel_put(img, iter.x, iter.y, color);
+	}
+}
