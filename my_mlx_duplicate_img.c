236f2a7d7bc20f7488e178ce3a95b5bf2b113721
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-05-19 18:24:46 +0100

    my_mlx_color_img.c function to deep copy the content of an image

---
+++ my_mlx_duplicate_img.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_duplicate_img.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 11:16:33 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/16 11:19:38 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "my_mlx.h"
+
+void	my_mlx_duplicate_img(void *mlx, t_img *dst, t_img src)
+{
+	if (!mlx || src.size.x <= 0 || src.size.y <= 0)
+		return ;
+	my_mlx_init_img(dst);
+	my_mlx_new_img(mlx, dst, (t_point){src.size.x, src.size.y});
+	if (!dst->img)
+		return ;
+	my_mlx_get_data_addr(dst);
+	if (!dst->addr)
+		my_mlx_destroy_img(mlx, dst);
+}
