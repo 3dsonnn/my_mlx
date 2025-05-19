f7783b94cf7e764603df3b1b9b39dfaeca349d7f
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-05-19 18:26:58 +0100

    my_mlx_get_data_addr.c function to get the address of the image and 'fix' the line len to the scaled to pixels

---
+++ my_mlx_get_data_addr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_get_data_addr.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 11:20:23 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/16 11:20:40 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "my_mlx.h"
+
+void	my_mlx_get_data_addr(t_img *img)
+{
+	if (!img || !img->img)
+		return ;
+	img->addr = (int *)mlx_get_data_addr(img->img, &img->bpp, &img->line_len,
+			&img->endian);
+	if (!img->addr)
+		return ;
+	img->line_len /= 4;
+}
