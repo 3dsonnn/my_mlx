d669f0f7a1fbaeedd4bb94a89c1404efc6bceea2
Edson Finda <efinda@c1r3s6.42luanda.com>
2025-05-19 18:41:35 +0100

    my_mlx_pixel_put.c function to put a pixel into a coordinate of the image passed as argument

---
+++ my_mlx_pixel_put.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_pixel_put.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efinda <efinda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 10:38:21 by efinda            #+#    #+#             */
+/*   Updated: 2025/05/16 10:39:50 by efinda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "my_mlx.h"
+
+inline void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+{
+	if (!img || !img->addr || x < 0 || x >= img->size.x || y < 0
+		|| y >= img->size.y)
+		return ;
+	*(img->addr + y * img->line_len + x) = color;
+}
